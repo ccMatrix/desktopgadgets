730576632365c46ee940fa39e26926ce37cf6721
codename.matrix <codename.matrix@e00a1d1f-563d-0410-a555-bbce3309907e>
2007-12-18 23:01:11 +0000

    + acoustic notifications + visual notifications + separate design choices for empty and full inbox * extended description for gadget

--- trunk/GMailIcon/options.js
+++ trunk/GMailIcon/options.js
@@ -15,39 +15,75 @@ function ShowOptionsDlg(wnd) {
 
 	wnd.AddControl(gddWndCtrlClassLabel, 0, "", strMoreOptions, 10, 150, 220, 20);
 
-  var options_account = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonCheck, "showAccount", strShowAccount, 20, 170, 260, 20);
-  options_account.value = options.getValue("displayAccount");
+  ctl = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonCheck, "showAccount", strShowAccount, 20, 170, 260, 20);
+  ctl.value = options.getValue("displayAccount");
 
-	var options_apps = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonCheck, "googleApps", strAppsFix, 20, 190, 260, 20);
-  options_apps.value = options.getValue("isGoogleApps");
+	ctl = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonCheck, "googleApps", strAppsFix, 20, 190, 260, 20);
+  ctl.value = options.getValue("isGoogleApps");
 
-	var options_alwaysacc = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonCheck, "alwaysAccount", strAlwaysAccount, 20, 210, 260, 20);
-  options_alwaysacc.value = options.getValue("alwaysAccount");
+	ctl = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonCheck, "alwaysAccount", strAlwaysAccount, 20, 210, 260, 20);
+  ctl.value = options.getValue("alwaysAccount");
 
 	wnd.AddControl(gddWndCtrlClassLabel, 0, "", strInterval, 20, 235, 80, 20);
   wnd.AddControl(gddWndCtrlClassEdit, gddWndCtrlClassEdit, "refreshInterval", options.getValue("interval")/(60*1000), 100, 230, 40, 25);
 	wnd.AddControl(gddWndCtrlClassLabel, 0, "", strMinutes, 145, 235, 80, 20);
 
 	var designs = [];
-	designs.push(strRedDesign);
-	designs.push(strGrayDesign);
-	designs.push(strGreenDesign)
-	designs.push(strBlueDesign);
-	ctl = wnd.AddControl(gddWndCtrlClassList, gddWndCtrlTypeListDrop, "design", designs, 20, 260, 260, 25);
+	var designAppendix = " ("+strDesignEmpty+")";
+	designs.push(strRedDesign+designAppendix);
+	designs.push(strGrayDesign+designAppendix);
+	designs.push(strGreenDesign+designAppendix)
+	designs.push(strBlueDesign+designAppendix);
+	ctl = wnd.AddControl(gddWndCtrlClassList, gddWndCtrlTypeListDrop, "designEmpty", designs, 20, 260, 260, 25);
 	switch (options.getValue("design")) {
 		case "red": 
-				ctl.value = strRedDesign;
+				ctl.value = strRedDesign+designAppendix;
 				break;
 		case "gray": 
-				ctl.value = strGrayDesign;
+				ctl.value = strGrayDesign+designAppendix;
 				break;
 		case "green": 
-				ctl.value = strGreenDesign;
+				ctl.value = strGreenDesign+designAppendix;
 				break;
 		case "blue": 
-				ctl.value = strBlueDesign;
+				ctl.value = strBlueDesign+designAppendix;
 				break;
 	}
+
+	designs = [];
+	designAppendix = " ("+strDesignFull+")";
+	designs.push(strRedDesign+designAppendix);
+	designs.push(strGrayDesign+designAppendix);
+	designs.push(strGreenDesign+designAppendix)
+	designs.push(strBlueDesign+designAppendix);
+	ctl = wnd.AddControl(gddWndCtrlClassList, gddWndCtrlTypeListDrop, "designFull", designs, 20, 285, 260, 25);
+	switch (options.getValue("designFull")) {
+		case "red": 
+				ctl.value = strRedDesign+designAppendix;
+				break;
+		case "gray": 
+				ctl.value = strGrayDesign+designAppendix;
+				break;
+		case "green": 
+				ctl.value = strGreenDesign+designAppendix;
+				break;
+		case "blue": 
+				ctl.value = strBlueDesign+designAppendix;
+				break;
+	}
+
+
+	wnd.AddControl(gddWndCtrlClassLabel, 0, "", strNotifyAlert, 36, 310, 240, 20);
+	ctl = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonPush, "notifyAlert", "?", 19, 310, 16, 16);
+	ctl.onClicked = OnAlertsInformation;
+
+	ctl = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonCheck, "notifySound", strNotifySound, 20, 330, 260, 20);
+  ctl.value = options.getValue("notifySoundEnable");
+  wnd.AddControl(gddWndCtrlClassEdit, 0, "notifySoundFile", options.getValue("notifySoundFile"), 40, 350, 190, 25);
+	ctl = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonPush, "chooseSound", "...", 235, 350, 25, 20);
+  ctl.onClicked = OnChooseSound;
+	ctl = wnd.AddControl(gddWndCtrlClassButton, gddWndCtrlTypeButtonPush, "previewSound", ">", 260, 350, 25, 20);
+  ctl.onClicked = OnPlaySound;
 
   wnd.onClose = OptionsDlgClosed;
   
@@ -78,30 +114,70 @@ function OptionsDlgClosed(wnd, code) {
 	ctl = wnd.GetControl("alwaysAccount");
 	options.putValue("alwaysAccount", ctl.value);
 
+	ctl = wnd.GetControl("notifySound");
+	options.putValue("notifySoundEnable", ctl.value);
+
+	ctl = wnd.GetControl("notifySoundFile");
+	options.putValue("notifySoundFile", ctl.value);
+
 	ctl = wnd.GetControl("refreshInterval");
 	options.putValue("interval", parseInt(ctl.value)*60*1000);
 
-	ctl = wnd.GetControl("design");
+	ctl = wnd.GetControl("designEmpty");
+	var designAppendix = " ("+strDesignEmpty+")";
 	switch (ctl.value) {
-		case strRedDesign: 
+		case strRedDesign+designAppendix: 
 				options.putValue("design", "red");
 				break;
-		case strGrayDesign: 
+		case strGrayDesign+designAppendix:
 				options.putValue("design", "gray");
 				break;
-		case strGreenDesign: 
+		case strGreenDesign+designAppendix:
 				options.putValue("design", "green");
 				break;
-		case strBlueDesign: 
+		case strBlueDesign+designAppendix:
 				options.putValue("design", "blue");
 				break;
 	}
-	debug.trace("Setting images based on selected design");
-	var design = options.getValue("design");
-	dot.src = "images\\"+design+"_dot.png";
-	titleBg.src = "images\\"+design+"_titlebg.png";
-	background.src = "images\\"+design+"_gmail_envelope.png";
+
+	ctl = wnd.GetControl("designFull");
+	var designAppendix = " ("+strDesignFull+")";
+	switch (ctl.value) {
+		case strRedDesign+designAppendix: 
+				options.putValue("designFull", "red");
+				break;
+		case strGrayDesign+designAppendix:
+				options.putValue("designFull", "gray");
+				break;
+		case strGreenDesign+designAppendix:
+				options.putValue("designFull", "green");
+				break;
+		case strBlueDesign+designAppendix:
+				options.putValue("designFull", "blue");
+				break;
+	}
 
 	checkMail();
   return true;
+}
+
+function OnChooseSound(wnd, control) {
+	var ctl = wnd.GetControl("notifySoundFile");
+	var audioFile = framework.BrowseForFile(strMusicFiles+"|*.mp3;*.wav");
+	if (audioFile.length > 0) {
+		ctl.value = audioFile
+	}
+}
+
+function OnPlaySound(wnd, control) {
+	var ctl = wnd.GetControl("notifySoundFile");
+	if (framework.system.filesystem.FileExists(ctl.value)) {
+		framework.audio.play(ctl.value);
+	}
+}
+
+function OnAlertsInformation(wnd, control) {
+	var ctl = wnd.GetControl("notifyAlert");
+	ctl.value = false;
+	alert( strAlertEnableInformation );
 }

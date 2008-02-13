d1b5010bccfb6bdde9fea750ca90087e1e5b7028
codename.matrix <codename.matrix@e00a1d1f-563d-0410-a555-bbce3309907e>
2008-02-13 22:47:13 +0000

    * additional testpages * more tags added * paragraphs and headers fixed + added scrollbars

--- HTMLRenderer/main.js
+++ HTMLRenderer/main.js
@@ -2,11 +2,26 @@
 webpages.push("http://www.codename-matrix.de/_private/example.html");
 webpages.push("http://www.december.com/html/demo/hello.html");
 webpages.push("http://www2.latech.edu/~acm/helloworld/HTML.html");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_basic");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_paragraphs1");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_paragraphs2");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_paragraphs");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_poem");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_headers");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_header");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_hr");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_comment");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_formattingch");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_pre");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_computeroutput");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_abbr");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_q");
+webpages.push("http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_basic");
 
 function view_onOpen() {
 	plugin.onAddCustomMenuItems = AddCustomMenuItems;
 
-	Render( "http://www.w3schools.com/html/tryit_view.asp?filename=tryhtml_link_locations" );
+	Render( webpages[0] );
 }
 
 function AddCustomMenuItems(menu) {

79256061c29c68f94bdcec950b8289d95293d375
codename.matrix <codename.matrix@e00a1d1f-563d-0410-a555-bbce3309907e>
2008-05-17 23:06:51 +0000

    CalendarPicker import

---
+++ trunk/CalendarPicker/main.js
@@ -0,0 +1,23 @@
+﻿function view_onOpen() {
+
+}
+
+function edit1_onrclick() {
+	var calendar = new CalendarPicker();
+	calendar.weekStart = 1;
+	calendar.onDatePicked = function() {
+		var d = calendar.value;
+		edit1.value = d.getMonth()+"/"+d.getDate()+"/"+d.getYear();
+		};
+	calendar.showAuto(edit1);  
+}
+
+function img1_onclick() {
+	var calendar = new CalendarPicker();
+	calendar.weekStart = 0;
+	calendar.onDatePicked = function() {
+		var d = calendar.value;
+		edit2.value = d.getMonth()+"/"+d.getDate()+"/"+d.getYear();
+		};
+	calendar.showAuto(img1);  
+}

617b994c01c6d1367fc79a3ab76e02ffbedd3e01
codename.matrix <codename.matrix@e00a1d1f-563d-0410-a555-bbce3309907e>
2008-09-15 00:44:12 +0000

    import of Separator gadget

---
+++ trunk/Separator/main.js
@@ -0,0 +1,14 @@
+function view_onOpen()
+ {
+
+
+}
+
+
+function fadeIn() {
+  beginAnimation("emptyDiv.opacity = event.value", emptyDiv.opacity, 80, 100);
+}
+
+function fadeOut() {
+  beginAnimation("emptyDiv.opacity = event.value", emptyDiv.opacity, 1, 100);
+}
